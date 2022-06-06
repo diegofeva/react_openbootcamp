42f76c4dd15da06c62b58b5b78543e6a23e364a7
Diego Fernández <diegofeva@gmail.com>
2022-06-06 12:00:14 +0200

    clase 4

--- proyect/my-app/src/App.js
+++ proyect/my-app/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Greeting from './components/pure/greeting';
 import GreetingF from './components/pure/greetingF';
 import TaskListComponent from './components/container/task_list';
+import Ejemplo1 from './hooks/Ejemplo1';
+import Ejemplo2 from './hooks/Ejemplo2';
+import MiComponenteConContexto from './hooks/Ejemplo3';
+import Ejemplo4 from './hooks/Ejemplo4';
 
 function App() {
   return (
@@ -17,8 +21,23 @@ function App() {
         {/* <GreetingF name={"Juan"}></GreetingF> */}
 
         {/*COMPONENTE DE LISTADO DE TAREAS*/}
-        <TaskListComponent ></TaskListComponent>
+        {/* <TaskListComponent ></TaskListComponent> */}
+
+        {/* Ejemplo de hooks useState */}
+        {/* <Ejemplo1></Ejemplo1> */}
+
+        {/* Ejemplo de useRef y useEffect */}
+        {/* <Ejemplo2></Ejemplo2> */}
+
+        {/* Ejemplo con useState y useContext */}
+        {/* <MiComponenteConContexto></MiComponenteConContexto> */}
         
+        <Ejemplo4 nombre="Martín">
+          {/* Todo lo que esta aqui contenido es tratado como props.children */}
+          <h3>
+            Contenido del props.children
+          </h3>
+        </Ejemplo4>
       </header>
     </div>
   );
